b0a0a3247b3a51e32e72eca2ab2ccfaa3d9fd6eb
darcivieira <darcivieira.junior@gmail.com>
2023-12-05 09:45:50 -0300

    Add docker containers

--- tests/test_challange.py
+++ tests/test_challange.py
@@ -1,11 +1,9 @@
-import pytest
+from time import sleep
 from challange_api import create_app
 from fastapi.testclient import TestClient
 from challange_api.generics.models import Model, engine
-from challange_api.helpers.make_transaction import MakeTransaction
 
 app = create_app()
-celery = app.celery_app
 
 client = TestClient(app)
 
@@ -31,8 +29,6 @@ USERS = [
     ]
 
 basic_data = {}
-
-# pytest_plugins = ("celery.contrib.pytest", )
 
 
 def create_users():
@@ -79,18 +75,49 @@ def test_check_and_update_first_common_user_wallet():
     assert response.json().get('value') == 1596.78
 
 
-@pytest.mark.celery(result_backend="rpc://")
-def test_first_common_user_send_money_to_shopkeeper():
+def test_first_common_user_send_money_to_shopkeeper(celery_session_app, celery_session_worker):
     response = client.post('/transactions', headers=basic_data.get('jose@gmail.com').get('headers'), json={"value": 156.0,"register_number": "12123456000189"})
     assert response.status_code == 201
     response = client.get('/wallets', headers=basic_data.get('jose@gmail.com').get('headers'))
     assert response.json().get('value') == 1440.78
 
 
-@pytest.mark.celery(result_backend="rpc://")
-def test_shopkeeper_user_send_money_to_shopkeeper():
+def test_shopkeeper_user_send_money_to_shopkeeper(celery_session_app, celery_session_worker):
+    sleep(15)
     response = client.get('/wallets', headers=basic_data.get('financeiro@empresa.com').get('headers'))
-    assert response.json().get('value') == 1440.78
+    assert response.json().get('value') == 156.0
     response = client.post('/transactions', headers=basic_data.get('financeiro@empresa.com').get('headers'), json={"value": 56.0,"register_number": "01234567890"})
     assert response.status_code == 401
+
+
+def test_second_common_user_send_money_to_shopkeeper(celery_session_app, celery_session_worker):
+    sleep(15)
+    response = client.get('/wallets', headers=basic_data.get('rodolfo@yahoo.com').get('headers'))
+    assert response.json().get('value') == 0
+    response = client.post('/transactions', headers=basic_data.get('rodolfo@yahoo.com').get('headers'), json={"value": 56.0,"register_number": "01234567890"})
+    assert response.status_code == 401
+    assert response.json() == {'detail': "You don't have enough balance!"}
+
+
+def test_first_common_user_send_money_to_second_common_user(celery_session_app, celery_session_worker):
+    sleep(15)
+    response = client.post('/transactions', headers=basic_data.get('jose@gmail.com').get('headers'), json={"value": 555.55,"register_number": "01234567890"})
+    assert response.status_code == 201
+
+
+def test_second_common_user_send_money_to_shopkeeper_second_time(celery_session_app, celery_session_worker):
+    sleep(15)
+    response = client.post('/transactions', headers=basic_data.get('rodolfo@yahoo.com').get('headers'), json={"value": 50.0,"register_number": "12123456000189"})
+    assert response.status_code == 201
+
+
+def test_money_in_the_wallets():
+    sleep(15)
+    response = client.get('/wallets', headers=basic_data.get('jose@gmail.com').get('headers'))
+    assert response.json().get('value') == 885.23
+    response = client.get('/wallets', headers=basic_data.get('rodolfo@yahoo.com').get('headers'))
+    assert response.json().get('value') == 505.55
+    response = client.get('/wallets', headers=basic_data.get('financeiro@empresa.com').get('headers'))
+    assert response.json().get('value') == 206.00
+
 
